0a4a46493917de9c424f6efa0637945766323945
sagar <sagarkoshti1990@gmail.com>
2020-01-23 00:28:19 +0530

    change in service provider

--- src/LaraCrudServiceProvider.php
+++ src/LaraCrudServiceProvider.php
@@ -13,7 +13,7 @@ class LaraCrudServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->make('sagar\laracrud\TodolistController');
     }
 
     /**
@@ -23,6 +23,11 @@ class LaraCrudServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->make('wisdmLabs\todolist\TodolistController');
+        $this->loadRoutesFrom(__DIR__.'/admin.php');
+        $this->loadRoutesFrom(__DIR__.'/base.php');
+        $this->loadMigrationsFrom(__DIR__.'/Migrations');
+        $this->publishes([
+            __DIR__.'/views' => base_path('View'),
+        ]);
     }
 }
